6a70c9da868cbe14be599f207854a09f65a36f4a
weilyuwang <weilyuwang@gmail.com>
2020-04-11 15:54:25 -0400

    apply some stylings

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -5,7 +5,7 @@ import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
     return (
-        <>
+        <View style={styles.container}>
             <Spacer>
                 <Text h3>Sign Up for Tracker</Text>
             </Spacer>
@@ -15,7 +15,7 @@ const SignupScreen = ({ navigation }) => {
             <Spacer>
                 <Button title="Sign Up" />
             </Spacer>
-        </>
+        </View>
     );
 };
 
@@ -38,6 +38,12 @@ SignupScreen.navigationOptions = () => {
 
 */
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        marginBottom: 300,
+    },
+});
 
 export default SignupScreen;
