d16cd764a213e23e778d25514941fb90b0296581
weilyuwang <weilyuwang@gmail.com>
2020-04-12 11:49:40 -0400

    show a map with inital region

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,9 +4,23 @@ import { Text } from "react-native-elements";
 import MapView from "react-native-maps";
 
 const Map = () => {
-    return <MapView />;
+    return (
+        <MapView
+            style={styles.map}
+            initialRegion={{
+                latitude: 43.779157850000004,
+                longitude: -79.41439751673187,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+            }}
+        />
+    );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    map: {
+        height: 300,
+    },
+});
 
 export default Map;
