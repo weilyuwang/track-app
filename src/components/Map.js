89b9b95158ae9cb7dd5f373c134d91cef7c58d58
weilyuwang <weilyuwang@gmail.com>
2020-04-12 11:55:25 -0400

    show track/line on the map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
-import MapView from "react-native-maps";
+import MapView, { Polyline } from "react-native-maps";
 
 const Map = () => {
+    let points = [];
+    for (let i = 0; i < 20; i++) {
+        points.push({
+            latitude: 43.779157850000004 + i * 0.001 * (Math.random() / 10),
+            longitude: -79.41439751673187 + i * 0.001,
+        });
+    }
+
     return (
         <MapView
             style={styles.map}
@@ -13,7 +21,9 @@ const Map = () => {
                 latitudeDelta: 0.01,
                 longitudeDelta: 0.01,
             }}
-        />
+        >
+            <Polyline coordinates={points} />
+        </MapView>
     );
 };
 
