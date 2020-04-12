2d6d6b0880d3e59056ea62357c140c477217eddb
weilyuwang <weilyuwang@gmail.com>
2020-04-12 16:10:23 -0400

    add LocationProvider to App

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import TrackListScreen from "./src/screens/TrackListScreen";
 import ResolveAuthScreen from "./src/screens/ResolveAuthScreen";
 
 import { Provider as AuthProvider } from "./src/context/AuthContext";
+import { Provider as LocationProvider } from "./src/context/LocationContext";
 import { setNavigator } from "./src/navigationRef";
 
 // will be loaded in the order they are placed in the navigator
@@ -37,12 +38,14 @@ const App = createAppContainer(switchNavigator);
 
 export default () => {
     return (
-        <AuthProvider>
-            <App
-                ref={(navigator) => {
-                    setNavigator(navigator);
-                }}
-            />
-        </AuthProvider>
+        <LocationProvider>
+            <AuthProvider>
+                <App
+                    ref={(navigator) => {
+                        setNavigator(navigator);
+                    }}
+                />
+            </AuthProvider>
+        </LocationProvider>
     );
 };
