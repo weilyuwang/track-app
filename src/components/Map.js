c3651cefb598fb2bd485c61bcaae91ec1253714f
weilyuwang <weilyuwang@gmail.com>
2020-04-12 11:43:33 -0400

    init Map component and start to work on TrackCreateScreen

---
+++ src/components/Map.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Text } from "react-native-elements";
+
+const Map = () => {
+    return <Text>I am a map</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default Map;
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { StyleSheet } from "react-native";
+import { Text } from "react-native-elements";
+import Map from "../components/Map";
+import { SafeAreaView } from "react-navigation";
 
 const TrackCreateScreen = () => {
-    return <Text style={{ fontSize: 48 }}>TrackCreateScreen</Text>;
+    return (
+        <SafeAreaView>
+            <Text h2>Create a Track</Text>
+            <Map />
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({});
