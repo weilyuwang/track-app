efdde3101df0bdf3feb52186a74f15919c7c693d
weilyuwang <weilyuwang@gmail.com>
2020-04-12 11:38:11 -0400

    some margin to account screen

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -3,20 +3,30 @@ import { View, StyleSheet } from "react-native";
 import { Button, Text } from "react-native-elements";
 import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/AuthContext";
+import { SafeAreaView } from "react-navigation";
 
 const AccountScreen = () => {
     const { signout } = useContext(AuthContext);
 
     return (
-        <>
-            <Text h1>Sign Out</Text>
+        <SafeAreaView forceInset={{ top: "always" }}>
+            <Text h1 style={styles.textStyle}>
+                Your Account
+            </Text>
+            <Spacer />
+            <Spacer />
             <Spacer>
                 <Button title="Sign Out" onPress={signout} />
             </Spacer>
-        </>
+        </SafeAreaView>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    textStyle: {
+        marginHorizontal: 16,
+        marginTop: 30,
+    },
+});
 
 export default AccountScreen;
