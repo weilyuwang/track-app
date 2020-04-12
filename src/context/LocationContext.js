b0dbe12f88f78d37f0495a102b1dd32d368453be
weilyuwang <weilyuwang@gmail.com>
2020-04-12 17:36:45 -0400

    add start&stop recording reducer handlers

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -4,17 +4,24 @@ const locationReducer = (state, action) => {
     switch (action.type) {
         case "add_current_location":
             return { ...state, currentLocation: action.payload };
+        case "start_recording":
+            return { ...state, recording: true };
+        case "stop_recording":
+            return { ...state, recording: false };
         default:
             return state;
     }
 };
 
-const startRecording = (dispatch) => () => {};
+const startRecording = (dispatch) => () => {
+    dispatch({ type: "start_recording" });
+};
 
-const stopRecording = (dispatch) => () => {};
+const stopRecording = (dispatch) => () => {
+    dispatch({ type: "stop_recording" });
+};
 
 const addLocation = (dispatch) => (location) => {
-    console.log("Updating User's current location...");
     dispatch({ type: "add_current_location", payload: location });
 };
 
