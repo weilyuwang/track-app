46961cf7877ca5971d115b11c8697ab52c067dea
weilyuwang <weilyuwang@gmail.com>
2020-04-13 12:25:53 -0400

    update axios api to add JWT to header automatically whenever a request is called.

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,8 +1,27 @@
 import axios from "axios";
 
-export default axios.create({
+import { AsyncStorage } from "react-native";
+
+const instance = axios.create({
     // the baseURL below points to Ngrok url, which redirects to express server.
     // note that the ngrok will change every 8 hours (session expired), we need to restart ngrok
     // and update the baseURL value every 8 hours
     baseURL: "http://99454017.ngrok.io",
 });
+
+// first function is called whenever we make a request,
+// second function is called whenever there is an error calling the request
+instance.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (err) => {
+        return Promise.reject(err);
+    }
+);
+
+export default instance;
