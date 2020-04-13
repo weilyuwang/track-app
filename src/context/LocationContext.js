ddba8fd1fe170202891194773d26a0a946b31e27
weilyuwang <weilyuwang@gmail.com>
2020-04-13 12:34:03 -0400

    reset locations and track name once user save the current tack

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -23,6 +23,12 @@ const locationReducer = (state, action) => {
                 ...state,
                 name: action.payload,
             };
+        case "reset":
+            return {
+                ...state,
+                name: "",
+                locations: [],
+            };
         default:
             return state;
     }
@@ -44,9 +50,13 @@ const addLocation = (dispatch) => (location) => {
     dispatch({ type: "add_location", payload: location });
 };
 
+const reset = (dispatch) => () => {
+    dispatch({ type: "reset" });
+};
+
 export const { Context, Provider } = createDataContext(
     locationReducer,
-    { startRecording, stopRecording, addLocation, changeName },
+    { startRecording, stopRecording, addLocation, changeName, reset },
     {
         recording: false,
         locations: [],
