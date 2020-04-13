eea4f75ebfe607be230b58fd0b1590e6ae377aaf
weilyuwang <weilyuwang@gmail.com>
2020-04-13 12:38:28 -0400

    implement fetchTracks action dispatcher

--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -3,12 +3,17 @@ import trackerApi from "../api/tracker";
 
 const trackReducer = (state, action) => {
     switch (action.type) {
+        case "fetch_tracks":
+            return action.payload;
         default:
             return state;
     }
 };
 
-const fetchTracks = (dispatch) => () => {};
+const fetchTracks = (dispatch) => async () => {
+    response = await trackerApi.get("/tracks");
+    dispatch({ type: "fetch_tracks", payload: response.data });
+};
 const createTrack = (dispatch) => async (name, locations) => {
     // make a request to our api
     await trackerApi.post("/tracks", { name, locations });
