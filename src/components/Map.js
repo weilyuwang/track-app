d0b7ce1ac82ae4d3593684a489563e9d01c7aad2
weilyuwang <weilyuwang@gmail.com>
2020-04-12 21:46:24 -0400

    add track on the map based on the locations array

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,7 +6,7 @@ import { Context as LocationContext } from "../context/LocationContext";
 
 const Map = () => {
     const {
-        state: { currentLocation },
+        state: { currentLocation, locations },
     } = useContext(LocationContext);
 
     if (!currentLocation) {
@@ -33,7 +33,9 @@ const Map = () => {
                 strokeColor="rgba(158,158, 255, 1.0)"
                 fillColor="rgba(158,158,255,0.3)"
             />
-            {/* <Polyline coordinates={points} /> */}
+            <Polyline
+                coordinates={locations.map((location) => location.coords)}
+            />
         </MapView>
     );
 };
