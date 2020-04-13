fdd444604efd28a9f4a410b000e60a0b6070892e
weilyuwang <weilyuwang@gmail.com>
2020-04-12 21:29:59 -0400

    fix bug

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -10,37 +10,46 @@ import {
 // and a boolean value shouldTrack to decide if we should continue recording user's current coordinates
 export default (shouldTrack, callback) => {
     const [error, setError] = useState(null);
-    const [subscriber, setSubscriber] = useState(null);
-
-    const startWatching = async () => {
-        try {
-            await requestPermissionsAsync();
-            // function to fetch user's current location data
-            // watchPositionAsync() creates a daemon thread that keeps running in the background
-            // until you call subscriber.remove() to unsubscribe/stop it
-            const sub = await watchPositionAsync(
-                {
-                    accuracy: Accuracy.BestForNavigation,
-                    timeInterval: 1000, // once every 1 second
-                    distanceInterval: 10, // once every 10 meters
-                },
-                callback // takes a location object: (location) => callback(location)
-            );
-            setSubscriber(sub);
-        } catch (err) {
-            // if user denies, the flow will fall to catch block
-            setError(err);
-        }
-    };
 
     useEffect(() => {
+        let subscriber;
+        const startWatching = async () => {
+            try {
+                await requestPermissionsAsync();
+                // function to fetch user's current location data
+                // watchPositionAsync() creates a daemon thread that keeps running in the background
+                // until you call subscriber.remove() to unsubscribe/stop it
+                subscriber = await watchPositionAsync(
+                    {
+                        accuracy: Accuracy.BestForNavigation,
+                        timeInterval: 500, // once every 0.5 second
+                        distanceInterval: 5, // once every 5 meters
+                    },
+                    callback // takes a location object: (location) => callback(location)
+                );
+            } catch (err) {
+                // if user denies, the flow will fall to catch block
+                setError(err);
+            }
+        };
+
         if (shouldTrack) {
             startWatching();
         } else {
             // stop watching
-            subscriber.remove();
-            setSubscriber(null);
+            if (subscriber) {
+                subscriber.remove();
+            }
+
+            subscriber = null;
         }
+
+        // clean up function
+        return () => {
+            if (subscriber) {
+                subscriber.remove();
+            }
+        };
     }, [shouldTrack]); // run startWatching() whenever shouldTrack(isFocused) changes
 
     // return error if necessary - make it to return an array for now, we could possibly return more stuff in the array in the future
